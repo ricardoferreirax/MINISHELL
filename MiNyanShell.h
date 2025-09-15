bc844ec90b10aa19ce34368922c7ab5baa3638fe
Pedro Augusto Freire Costa <pfreire-@c2r9s6.42lisboa.com>
2025-09-15 17:05:24 +0100

    added out file and infile to struct s_0w0

--- MiNyanShell.h
+++ MiNyanShell.h
@@ -6,47 +6,47 @@
 /*   By: pfreire- <pfreire-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 15:19:52 by pfreire-          #+#    #+#             */
-/*   Updated: 2025/09/15 16:34:15 by pfreire-         ###   ########.fr       */
+/*   Updated: 2025/09/15 16:46:28 by pfreire-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINYANSHELL_H
-#define MINYANSHELL_H
+# define MINYANSHELL_H
 
-#include "libft/libft.h"
+# include "libft/libft.h"
 
-//Base Structs, hand s off dude, no touching these
+// Base Structs, hand s off dude, no touching these
 
-typedef struct s_OwO;
+typedef struct	s_OwO;
 
 typedef struct s_0w0
 {
-	bool hero_doc;
-	bool append;
-	char *delimiter;
-	char **args;
-	char *cmd;
-	s_0w0 *next;
-	s_OwO *last;
-}	t_subcmd;
+	bool		hero_doc;
+	bool		append;
+	char		*delimiter;
+	char		**args;
+	char		*cmd;
+	char		*infile;
+	char		*outfile;
+	s_0w0		*next;
+	s_OwO		*last;
+}				t_subcmd;
 
 typedef struct s_OwO
 {
-	bool pipe;
-	t_subcmd *head;
-	s_OwO *next;
-}	t_cmd;
+	bool		pipe;
+	t_subcmd	*head;
+	s_OwO		*next;
+}				t_cmd;
 
-//base structs end
+// base structs end
 
 typedef struct s_UwU
 {
-	t_cmd *head;
-	t_cmd *tail;
-	char **env;
-	
-}	t_mini;
+	t_cmd		*head;
+	t_cmd		*tail;
+	char		**env;
 
-
+}				t_mini;
 
 #endif
