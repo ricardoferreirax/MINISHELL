088a85710274eabb056c150995b3022b5d1b05d7
Pedro Augusto Freire Costa <pfreire-@c2r7s11.42lisboa.com>
2025-09-16 16:51:30 +0100

    Reseted

--- MiNyanShell.h
+++ MiNyanShell.h
@@ -6,47 +6,51 @@
 /*   By: pfreire- <pfreire-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 15:19:52 by pfreire-          #+#    #+#             */
-/*   Updated: 2025/09/15 16:46:28 by pfreire-         ###   ########.fr       */
+/*   Updated: 2025/09/16 14:30:09 by pfreire-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINYANSHELL_H
 # define MINYANSHELL_H
 
-# include "libft/libft.h"
-
+# include "parsing/parsing.h"
+# include "stdio.h"
+# include <readline/history.h>
+# include <readline/readline.h>
 // Base Structs, hand s off dude, no touching these
+// OK
 
-typedef struct	s_OwO;
+typedef struct s_OwO	t_cmd;
+typedef struct s_0w0	t_subcmd;
 
 typedef struct s_0w0
 {
-	bool		hero_doc;
-	bool		append;
-	char		*delimiter;
-	char		**args;
-	char		*cmd;
-	char		*infile;
-	char		*outfile;
-	s_0w0		*next;
-	s_OwO		*last;
-}				t_subcmd;
+	bool				hero_doc;
+	bool				append;
+	char				*delimiter;
+	char				**args;
+	char				*cmd;
+	char				*infile;
+	char				*outfile;
+	t_subcmd			*next;
+	t_cmd				*last;
+}						t_subcmd;
 
 typedef struct s_OwO
 {
-	bool		pipe;
-	t_subcmd	*head;
-	s_OwO		*next;
-}				t_cmd;
+	bool				pipe;
+	t_subcmd			*head;
+	t_cmd				*next;
+}						t_cmd;
 
 // base structs end
 
 typedef struct s_UwU
 {
-	t_cmd		*head;
-	t_cmd		*tail;
-	char		**env;
+	t_cmd				*head;
+	t_cmd				*tail;
+	char				**env;
 
-}				t_mini;
+}						t_mini;
 
 #endif
