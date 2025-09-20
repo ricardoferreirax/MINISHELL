28854d6919e1f8dd383b0f394800893a4a3f4b22
Ricardo <ricardomedeirosx@gmail.com>
2025-09-20 16:09:05 +0100

    split the handle_redirs function/split the main execution function to deal separatedly the builts in the parent and in the child

--- execution/builtin.c
+++ execution/builtin.c
@@ -6,30 +6,46 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 19:07:22 by rmedeiro          #+#    #+#             */
-/*   Updated: 2025/09/19 19:09:19 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2025/09/20 15:42:20 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MiNyanShell.h"
 
-int is_bultin(char *cmd)
+bool	is_builtin(const char *cmd)
 {
-    if (!cmd)
-        return (0);
-    if (strcmp(cmd, "cd") == 0)
-        return (1);
-    if (strcmp(cmd, "echo") == 0)
-        return (1);
-    if (strcmp(cmd, "env") == 0)
-        return (1);
-    if (strcmp(cmd, "exit") == 0)
-        return (1);
-    if (strcmp(cmd, "export") == 0)
-        return (1);
-    if (strcmp(cmd, "pwd") == 0)
-        return (1);
-    if (strcmp(cmd, "unset") == 0)
-        return (1);
-    return (0);
+	if (!cmd)
+		return (false);
+	if (ft_strcmp(cmd, "cd") == 0
+		|| ft_strcmp(cmd, "echo") == 0
+		|| ft_strcmp(cmd, "env") == 0
+		|| ft_strcmp(cmd, "exit") == 0
+		|| ft_strcmp(cmd, "export") == 0
+		|| ft_strcmp(cmd, "pwd") == 0
+		|| ft_strcmp(cmd, "unset") == 0)
+		return (true);
+	return (false);
 }
 
+bool builtin_runs_in_parent(t_subcmd *subcmd)
+{
+    if (!subcmd || !subcmd->args || !subcmd->args[0])
+        return (false);
+    if (ft_strcmp(subcmd->args[0], "cd") == 0
+        || ft_strcmp(subcmd->args[0], "export") == 0
+        || ft_strcmp(subcmd->args[0], "unset") == 0
+        || ft_strcmp(subcmd->args[0], "exit") == 0)
+        return (true);
+    return (false);
+}
+
+// bool builtin_runs_in_child(t_subcmd *subcmd)
+// {
+//     if (!subcmd || !subcmd->args || !subcmd->args[0])
+//         return false;
+//     if (ft_strcmp(subcmd->args[0], "echo") == 0
+//         || ft_strcmp(subcmd->args[0], "pwd") == 0
+//         || ft_strcmp(subcmd->args[0], "env") == 0)
+//         return true;
+//     return false;
+// }
--- execution/redirection.c
+++ execution/redirection.c
@@ -6,45 +6,58 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 18:37:56 by rmedeiro          #+#    #+#             */
-/*   Updated: 2025/09/19 14:22:29 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2025/09/20 15:53:28 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MiNyanShell.h"
+#include "execution.h"
+
+static void handle_redir_in(t_redir *redir)
+{
+    int fd = open(redir->file, O_RDONLY);
+    if (fd < 0)
+        error_exit("minishell: infile open failed");
+    safe_dup2_and_close(fd, STDIN_FILENO);
+}
+
+static void handle_redir_out(t_redir *redir)
+{
+    int fd = open(redir->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd < 0)
+        error_exit("minishell: outfile open failed");
+    safe_dup2_and_close(fd, STDOUT_FILENO);
+}
+
+static void handle_redir_append(t_redir *redir)
+{
+    int fd = open(redir->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fd < 0)
+        error_exit("minishell: outfile append open failed");
+    safe_dup2_and_close(fd, STDOUT_FILENO);
+}
+
+static void handle_redir_heredoc(t_subcmd *subcmd)
+{
+    safe_dup2_and_close(subcmd->in_fd, STDIN_FILENO);
+}
 
 void handle_redirs(t_subcmd *subcmd)
 {
-	t_redir *redir;
-	int fd;
+    t_redir *redir = subcmd->redirs;
 
-	redir = subcmd->redirs;
-	while (redir)
-	{
-		if (redir->type == REDIR_IN)
-		{
-			fd = open(redir->file, O_RDONLY);
-			if (fd < 0)
-				error_exit("minishell: infile open failed");
-			safe_dup2_and_close(fd, STDIN_FILENO);
-		}
-		else if (redir->type == REDIR_OUT)
-		{
-			fd = open(redir->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if (fd < 0)
-				error_exit("minishell: outfile open failed");
-			safe_dup2_and_close(fd, STDOUT_FILENO);
-		}
-		else if (redir->type == REDIR_APPEND)
-		{
-			fd = open(redir->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-			if (fd < 0)
-				error_exit("minishell: outfile append open failed");
-			safe_dup2_and_close(fd, STDOUT_FILENO);
-		}
-		else if (redir->type == REDIR_HEREDOC)
-			safe_dup2_and_close(subcmd->in_fd, STDIN_FILENO);
-		else if (redir->type == REDIR_INVALID)
-			error_exit("minishell: invalid redirection type");
-		redir = redir->next;
-	}
+    while (redir)
+    {
+        if (redir->type == REDIR_IN)
+            handle_redir_in(redir);
+        else if (redir->type == REDIR_OUT)
+            handle_redir_out(redir);
+        else if (redir->type == REDIR_APPEND)
+            handle_redir_append(redir);
+        else if (redir->type == REDIR_HEREDOC)
+            handle_redir_heredoc(subcmd);
+        else if (redir->type == REDIR_INVALID)
+            error_exit("minishell: invalid redirection type");
+        redir = redir->next;
+    }
 }
