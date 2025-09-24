0d38049597417af4c48f9af7270e27be37acf964
Ricardo <ricardomedeirosx@gmail.com>
2025-09-24 13:45:36 +0100

    send echo

--- builtin/ft_echo.c
+++ builtin/ft_echo.c
@@ -6,9 +6,32 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 11:47:43 by rmedeiro          #+#    #+#             */
-/*   Updated: 2025/09/23 22:29:07 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2025/09/23 23:45:58 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../MiNyanShell.h"
 
+int	ft_echo(char **args)
+{
+	int	i;
+	int	no_newline;
+
+	i = 1;
+	no_newline = 0;
+	if (args[1] && ft_strncmp(args[1], "-n", 2) == 0)
+	{
+		no_newline = 1;
+		i++;
+	}
+	while (args[i])
+	{
+		write(STDOUT_FILENO, args[i], ft_strlen(args[i]));
+		if (args[i + 1])
+			write(STDOUT_FILENO, " ", 1);
+		i++;
+	}
+	if (!no_newline)
+		write(STDOUT_FILENO, "\n", 1);
+	return (EXIT_SUCCESS);
+}
