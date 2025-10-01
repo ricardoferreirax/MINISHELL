703723954bbbef114c48aff820ad6f71e672db51
P3dr0-1702 <supermegagamerprofissional@gmail.com>
2025-10-01 16:48:09 +0100

    started pre-parsing

--- parsing/init_fill_mini.c
+++ parsing/init_fill_mini.c
@@ -6,12 +6,14 @@
 /*   By: pfreire- <pfreire-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/28 23:29:54 by rmedeiro          #+#    #+#             */
-/*   Updated: 2025/10/01 14:34:52 by pfreire-         ###   ########.fr       */
+/*   Updated: 2025/10/01 16:43:14 by pfreire-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../MiNyanShell.h"
 #include "parsing.h"
+#include <readline/readline.h>
+#include <string.h>
 
 t_cmd	*cmd_new(void);
 int		arr_size(void **arr);
@@ -46,20 +48,88 @@ char	**init_mini(t_mini *nyan, char *cmd)
 	return (pipes);
 }
 
-int pre_parse_word_count(char *str)
+int	expand_size(char *str, int i)
 {
-	
+	//must return the size of the expanded string $XXXX 
+	(void) str;
+	return (i - i);
 }
 
-char **pre_parse(char *pipe)
+int	pre_parse_size_count(char *str)
 {
-	int i = 0;
-	char **dest;
-	dest = malloc(sizeof(char *) * pre_parse_word_count(pipe));
-	while(pipe[i])
+	int		i;
+	int		size;
+	bool	inquote;
+	bool	indquote;
+
+	i = 0;
+	size = 0;
+	inquote = false;
+	indquote = false;
+	while (str[i] != '\0')
 	{
-		
+		if (str[i] == '\'' && !indquote)
+			inquote = !inquote;
+		if (str[i] == '\"' && !inquote)
+			indquote = !indquote;
+		if (str[i] == '<' || str[i] == '>')
+		{
+			if ((!indquote && !indquote) && (str[i - 1] != ' ' || str[i - 1] != '>' || str[i - 1] != '<'))
+				size++;
+			if ((!indquote && !inquote) && (str[i + 1] != ' ' || str[i + 1] != '>' || str[i + 1] != '<'))
+				size++;
+		}
+		if (!inquote && str[i] == '$')
+			size += expand_size(str, i);
+		i++;
+		size++;
 	}
+	return (size);
+}
+
+char	**pre_parse(char *pipe)
+{
+	int		i;
+	char	*dest;
+	bool	indquote;
+	bool	inquote;
+	char	**final;
+
+	i = 0;
+	indquote = false;
+	inquote = false;
+	int j = 0;
+	dest = malloc(sizeof(char *) * pre_parse_size_count(pipe));
+	while (pipe[i])
+	{
+		if (pipe[i] == '\'' && !indquote)
+			inquote = !inquote;
+		if (pipe[i] == '\"' && !inquote)
+			indquote = !indquote;
+		if (pipe[i] == '<' || pipe[i] == '>')
+		{
+			if ((!inquote && !indquote) && (pipe[i - 1] != ' ' || pipe[i - 1] != '>' || pipe[i - 1] != '<'))
+			{
+				dest[j + i] = ' ';
+				j++;
+			}
+			if ((!inquote && !indquote) && (pipe[i + 1] != ' ' || pipe[i + 1] != '>' || pipe[i + 1] != '<'))
+			{
+				dest[i + j] = ' ';
+				j++;
+			}
+		}
+		if (!inquote && pipe[i] == '$')
+		{
+			//must copy the expanded string $XXXX
+			j += ft_strlcat(dest, dest, 100);
+		}
+		dest[i + j] = pipe[i];
+		i++;
+	}
+	final = split_ignore_quotes(dest, ' ');
+	free(dest);
+	return (final);
 }
 
 void	fill_mini(t_mini *nyan, char **pipes)
