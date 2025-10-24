7c20f42b2bc5864627fc19d6b3584d5a16c4358d
P3dr0-1702 <supermegagamerprofissional@gmail.com>
2025-10-24 15:57:51 +0100

    Fixed adding spaces correctly

--- parsing/init_fill_mini.c
+++ parsing/init_fill_mini.c
@@ -18,7 +18,7 @@ t_cmd	*cmd_new(void)
 {
 	t_cmd	*cmd;
 
-	cmd = (t_cmd *)calloc(1, sizeof(*cmd));
+	cmd = (t_cmd *)ft_calloc(1, sizeof(*cmd));
 	if (!cmd)
 		return (NULL);
 	cmd->args = NULL;
@@ -58,14 +58,7 @@ char	**init_mini(t_mini *nyan, char *cmd)
 	return (pipes);
 }
 
-int	expand_size(char *str, int i)
-{
-	// must return the size of the expanded string $XXXX
-	(void)str;
-	return (i - i);
-}
-
-int	pre_parse_size_count(char *str)
+int	add_spaces_size_count(char *str)
 {
 	int		i;
 	int		size;
@@ -91,72 +84,72 @@ int	pre_parse_size_count(char *str)
 					+ 1] != '>' || str[i + 1] != '<'))
 				size++;
 		}
-		if (!inquote && str[i] == '$')
-			size += expand_size(str, i);
 		i++;
 		size++;
 	}
 	return (size);
 }
 
-char	*expander_helper(char *str, t_envyan *envyan)
-{
-	char	*expanded;
+//char	*str_expand(char *str, t_envyan env)
+//{
+//	int		i;
+//	int		size;
+//	char	*dest;
+//	// need to alocate enough memory for the var_name;
+//	char	*var_name = NULL;
+//	int		j;
+//
+//	i = 0;
+//	size = 0;
+//	while (str[i] != '\0')
+//	{
+//		if (str[i] == '$')
+//		{
+//			if (str[i + 1] == '?')
+//			{
+//			}
+//			else
+//			{
+//				i++;
+//				j = 0;
+//				while (ft_isalpha(str[i + j]) || ft_isdigit(str[i + j]) || str[i
+//					+ j] == '_')
+//						j++;
+//				var_name = malloc(sizeof(char) * j);
+//			}
+//		}
+//		size++;
+//	}
+//	return (dest);
+//}
+//// below it is wrong. it doesn't hangel echo h$USER where the ouput is hpedro or echo h"ello" where the output is hello
+//char	**expanser(t_cmd *cmd, t_envyan *envyan)
+//{
+//	int		i;
+//	bool	inquote;
+//	int		j;
+//
+//	while (cmd)
+//	{
+//		i = 0;
+//		while (cmd->args[i] != NULL)
+//		{
+//			j = 0;
+//			if (cmd->args[i][j] == '\'')
+//				inquote = !inquote;
+//			while (cmd->args[i][j] != '\0')
+//			{
+//				if (cmd->args[i][j] == '$')
+//				{
+//					// copy everything after $ and pass it to a function to return the desired string
+//					// then copy everything to the cmd->args
+//				}
+//			}
+//		}
+//	}
+//}
 
-	while (envyan != NULL)
-	{
-		if (ft_strcmp(envyan->key, str) == 0)
-		{
-
-			expanded = ft_strdup(envyan->value);
-			return (expanded);
-		}
-		envyan = envyan->next;
-	}
-	ft_printf("didn't find in PATH");
-	sleep(1);
-	expanded = ft_strdup("");
-	return (expanded);
-}
-
-
-//below it is wrong. it doesn't hangel echo h$USER where the ouput is hpedro or echo h"ello" hwere the output is 
-char	**expanser(char *str, t_envyan *envyan)
-{
-	char	**out;
-	int		i;
-	char	*temp;
-
-	out = split_ignore_quotes(str, ' ');
-	i = 0;
-	while (out[i] != NULL)
-	{
-		if (out[i][0] == '$')
-		{
-			temp = ft_strdup(out[i]);
-			free(out);
-			out[i] = expander_helper(temp, envyan);
-		}
-		else if(out[i][0] == '\"')
-		{
-			temp = ft_strdup(out[i]);
-			free(out[i]);
-			unsigned int k = 1;
-			out[i] = malloc(sizeof(char) * (ft_strlen(temp) - 2));
-			while(k < (ft_strlen(temp) - 1))
-			{
-				out[i][k - 1] = temp[k];
-				k++;
-			}
-			out[i][k] = '\0';
-			free(temp);
-		}
-		i++;
-	}
-	return (out);
-}
-
-char	**pre_parse(char *pipe, t_envyan envyan)
+char	**add_spaces(char *pipe)
 {
 	int		i;
 	char	*dest;
@@ -169,8 +162,7 @@ char	**pre_parse(char *pipe, t_envyan envyan)
 	indquote = false;
 	inquote = false;
 	j = 0;
-	dest = malloc(sizeof(char *) * pre_parse_size_count(pipe) + 1);
-	// printf("Size of pipe: %d\n", pre_parse_size_count(pipe));
+	dest = malloc(sizeof(char) * add_spaces_size_count(pipe) + 1);
 	while (pipe[i])
 	{
 		if (pipe[i] == '\'' && !indquote)
@@ -187,20 +179,19 @@ char	**pre_parse(char *pipe, t_envyan envyan)
 			}
 			dest[j + i] = pipe[i];
 			i++;
-			if ((!inquote && !indquote) && (pipe[i + 1] != ' ' && pipe[i
-					+ 1] != '>' && pipe[i + 1] != '<'))
+			if ((!inquote && !indquote) && (pipe[i] != ' ' && pipe[i] != '>' && pipe[i] != '<'))
 			{
 				dest[j + i] = ' ';
 				j++;
 			}
+			continue;
 		}
 		dest[i + j] = pipe[i];
 		i++;
 	}
 	dest[i + j] = '\0';
+	ft_printf("%s \n", dest);
 	final = split_ignore_quotes(dest, ' ');
-	//cannot cuking expand here bitch
-//	final = expanser(dest, &envyan);
 	free(dest);
 	return (final);
 }
@@ -216,8 +207,7 @@ void	fill_mini(t_mini *nyan, char **pipes)
 	i = 0;
 	while (pipes && pipes[i] && curr)
 	{
-		tokens = pre_parse(pipes[i], *nyan->envyan);
-		// tokens = split_ignore_quotes(pipes[i], ' ');
+		tokens = add_spaces(pipes[i]);
 		j = 0;
 		while (tokens[j] != NULL)
 		{
@@ -229,6 +219,6 @@ void	fill_mini(t_mini *nyan, char **pipes)
 		curr = curr->next;
 		i++;
 	}
-	//expnade HERE and do it with the list mf
-	//
+	// expnade HERE and do it with the list mf
+	//expanser(curr, nyan->envyan);
 }
