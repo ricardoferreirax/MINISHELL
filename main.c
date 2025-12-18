e3a28948a24e4f6d9c9733d3f075566ec5d35842
P3dr0-1702 <supermegagamerprofissional@gmail.com>
2025-12-18 12:36:49 +0000

    main's norme is done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 20:18:06 by rmedeiro          #+#    #+#             */
-/*   Updated: 2025/12/18 11:38:24 by pfreire-         ###   ########.fr       */
+/*   Updated: 2025/12/18 12:36:33 by pfreire-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,54 +15,41 @@
 #include "include/execution.h"
 #include "include/parsing.h"
 #include "include/signals.h"
+#include "print/dprintf/ft_dprintf.h"
+#include "print/printf/ft_printf.h"
 #include <readline/history.h>
 #include <readline/readline.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-static void	process_line(t_mini *mini, char *input)
+static int	process_line(t_mini *mini, char *input)
 {
 	char	**pipes;
 	int		status;
-	int		k;
 
-	k = 0;
+	status = 0;
 	if (!input || !mini)
-		return ;
+		return (-1);
 	if (*input)
 		add_history(input);
 	if (!no_unclosed_quotes(input))
-	{
-		ft_dprintf(2, "MiNyanShell: syntax error: unclosed quotes\n");
-		return ;
-	}
+		return (ft_dprintf(2, "MiNyanShell: swyntax wewor: unclosed quotes \n")
+			- 1);
 	if (!no_forbidden_actions(input))
-	{
-		ft_dprintf(2, "MiNyanShell: unsupported operator (&&, ||, *)\n");
-		return ;
-	}
+		return (ft_dprintf(2, "MiNyanShell: unsuwuppowted opewator (&&, ||,
+				*)\n"), -1);
 	if (!good_syntax(input))
-	{
-		ft_dprintf(2, "invalid Syntax nyan~\n");
-		return ;
-	}
+		return (ft_dprintf(2, "inwalid Swyntax nyan~\n"), -1);
 	pipes = init_mini(mini, input);
 	if (!pipes)
-	{
-		perror("init_mini");
-		return ;
-	}
-	if ((k = fill_mini(mini, pipes)))
-	{
-		ft_printf("Fuuuuuuuck something broke here i hope it doesn't leak\n");
-	}
+		return (perror("init_mini"), -1);
+	status = fill_mini(mini, pipes);
 	free_2d((void **)pipes);
-	if (k)
-		status = k;
-	else
+	if (!status)
 		status = execute_pipeline(mini->head, mini);
 	mini->last_status = status;
+	return (0);
 }
 
 static void	command_loop(t_mini *mini)
@@ -71,8 +58,7 @@ static void	command_loop(t_mini *mini)
 
 	while (1)
 	{
-		input = readline("MiNyanShell> ");
-		// the strcmp bellow is for testing only, must be removed
+		input = readline("MiNyanShell nyan~ :3 > ");
 		if (!input || ft_strcmp(input, "exit") == 0)
 		{
 			printf("\nexit\n");
@@ -118,15 +104,16 @@ int	main(int ac, char **av, char **envp)
 
 	if (ac != 1)
 	{
-		if (av && av[1])
-			ft_dprintf(2, "MiNyanShell: %s: Invalid argument\n", av[1]);
-		else
-			ft_dprintf(2, "MiNyanShell: Invalid argument\n");
-		return (1);
+		ft_dprintf(2, "MiNyanShell: %s: Invalid argument\n", av[1]);
+		ft_dprintf(2, "What do you expect me to do? ╮(╯_╰)╭o");
+		return (-1);
 	}
 	if (print_MiNyanShell())
-		return (ft_printf("Missing critical MiNyanShell.txt file,
-				refusing to continue\n"), -1);
+	{
+		ft_dprintf(2, "Miwing critical MiNyanShell.txt file ");
+		ft_dprintf(2, "(╥﹏╥)");
+		return (ft_dprintf(2, " wefusing to continue\n"), -1);
+	}
 	init_shell(&mini, envp);
 	command_loop(&mini);
 	minyanshell_exit_cleanup(&mini);
