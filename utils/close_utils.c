705d932781544065351672487cbc7357f123e562
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2025-12-19 06:21:56 +0000

    Norminetted close_utils.c

--- utils/close_utils.c
+++ utils/close_utils.c
@@ -13,57 +13,57 @@
 #include "../include/MiNyanShell.h"
 #include "../include/execution.h"
 
-void close_heredoc(t_cmd *cmd)
+void	close_heredoc(t_cmd *cmd)
 {
-    if (!cmd)
-        return ;
-    if (cmd->in_fd != -1)
-    {
-        close_fd_safe(&cmd->in_fd);
-        cmd->in_fd = -1;
-    }
+	if (!cmd)
+		return ;
+	if (cmd->in_fd != -1)
+	{
+		close_fd_safe(&cmd->in_fd);
+		cmd->in_fd = -1;
+	}
 }
 
-void close_fd_safe(int *fd)
+void	close_fd_safe(int *fd)
 {
-    if (fd && *fd != -1)
-    {
-        close(*fd);
-        *fd = -1;
-    }
+	if (fd && *fd != -1)
+	{
+		close(*fd);
+		*fd = -1;
+	}
 }
 
-void close_all_heredoc_fds(t_cmd *head)
+void	close_all_heredoc_fds(t_cmd *head)
 {
-    t_cmd *current;
+	t_cmd	*current;
 
 	current = head;
-    while (current)
-    {
-        if (current->in_fd != -1) 
-        { 
-            close(current->in_fd); 
-            current->in_fd = -1; 
-        }
-        current = current->next;
-    }
+	while (current)
+	{
+		if (current->in_fd != -1)
+		{
+			close(current->in_fd);
+			current->in_fd = -1;
+		}
+		current = current->next;
+	}
 }
 
-void close_my_pipes(t_pipe_pos pos, t_pipeline *pp)
+void	close_my_pipes(t_pipe_pos pos, t_pipeline *pp)
 {
-    if (!pp)
-        return;
-    if (pos == FIRST)
-    {
-        close_fd_safe(&pp->pipefd[0]);
-        close_fd_safe(&pp->pipefd[1]);
-    }
-    else if (pos == MIDDLE)
-    {
-        close_fd_safe(&pp->prev_pipefd);
-        close_fd_safe(&pp->pipefd[0]);
-        close_fd_safe(&pp->pipefd[1]);
-    }
-    else if (pos == LAST)
-        close_fd_safe(&pp->prev_pipefd);
+	if (!pp)
+		return ;
+	if (pos == FIRST)
+	{
+		close_fd_safe(&pp->pipefd[0]);
+		close_fd_safe(&pp->pipefd[1]);
+	}
+	else if (pos == MIDDLE)
+	{
+		close_fd_safe(&pp->prev_pipefd);
+		close_fd_safe(&pp->pipefd[0]);
+		close_fd_safe(&pp->pipefd[1]);
+	}
+	else if (pos == LAST)
+		close_fd_safe(&pp->prev_pipefd);
 }
